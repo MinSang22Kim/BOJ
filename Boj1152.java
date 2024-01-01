4ad6cd964ceb4d4323b56b54ec9c9dca60003306
MinSang22Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-01-01 22:20:09 +0900

    Add files via upload

---
+++ Boj1152.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Boj1152 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String sentence = scan.nextLine();
+
+		StringTokenizer st = new StringTokenizer(sentence, " ");
+		System.out.println(st.countTokens());
+
+		scan.close();
+	}
+}
