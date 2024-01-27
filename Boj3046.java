f36b944f763f92bd0ee7a53849dddbcf1e96e4bd
MinSang22Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-01-27 19:43:04 +0900

    Add files via upload

---
+++ Boj3046.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj3046 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int R1 = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+
+		bw.write(Integer.toString(2 * S - R1));
+		bw.flush();
+		bw.close();
+	}
+}
