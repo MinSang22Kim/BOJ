388c6dd049d8bd635f82199cd6e3a4d299b807be
MinSang22Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-02-12 15:01:29 +0900

    Add files via upload

---
+++ Boj11651.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj11651 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, (e1, e2) -> {
+			if (e1[1] == e2[1])
+				return e1[0] - e2[0];
+			else {
+				return e1[1] - e2[1];
+			}
+		});
+
+		for (int i = 0; i < N; i++) {
+			bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
