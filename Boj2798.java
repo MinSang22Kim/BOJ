237fa8467f5db59ee6fa9e75c5ee3c3395fe349a
MinSang22Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-02-13 19:15:18 +0900

    Add files via upload

---
+++ Boj2798.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj2798 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine(), " ");
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int result = 0;
+
+		for (int i = 0; i < arr.length - 2; i++) {
+			for (int j = i + 1; j < arr.length - 1; j++) {
+				for (int k = j + 1; k < arr.length; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					if (sum <= M)
+						result = Math.max(result, sum);
+				}
+			}
+		}
+
+		bw.write(Integer.toString(result));
+		bw.flush();
+		bw.close();
+	}
+}
